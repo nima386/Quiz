1195994af89c6cf5a59914f5f5078d82b27d37b4
nima386 <re.nima786@gmail.com>
2026-04-25 16:26:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,18 +12,18 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  if (event.request.url.includes("questions.json")) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
